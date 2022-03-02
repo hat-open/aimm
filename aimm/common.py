5799eb6400f352a389a73c52979c9e4b28b1303b
zlatsic <zlatan.sicanica@gmail.com>
2022-03-02 14:17:06 +0100

    common - json schema is optional, error log if fail

--- aimm/common.py
+++ aimm/common.py
@@ -2,16 +2,26 @@ from hat import json
 from hat import util
 from pathlib import Path
 import hat.monitor.common
+import logging
 import typing
+
+
+mlog = logging.getLogger(__name__)
 
 
 package_path: Path = Path(__file__).parent
 """Package file system path"""
 
-json_schema_repo: json.SchemaRepository = json.SchemaRepository(
-    hat.monitor.common.json_schema_repo,
-    json.SchemaRepository.from_json(package_path / 'json_schema_repo.json'))
-"""JSON schema repository"""
+try:
+    json_schema_repo: json.SchemaRepository = json.SchemaRepository(
+        hat.monitor.common.json_schema_repo,
+        json.SchemaRepository.from_json(
+            package_path / 'json_schema_repo.json'))
+    """JSON schema repository"""
+except Exception as e:
+    mlog.error('could not load JSON schema, AIMM will not work properly: %e',
+               e, exc_info=e)
+    json_schema_repo: json.SchemaRepository({})
 
 
 JSON = typing.Union[None, bool, int, float, str, typing.List['JSON'],
