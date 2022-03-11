2f020e66bfcf69d1ebf3012106dff2eb21b8be9e
zlatsic <zlatan.sicanica@gmail.com>
2022-03-11 11:20:55 +0100

    aimm common - remove fallback schema

--- aimm/common.py
+++ aimm/common.py
@@ -12,17 +12,11 @@ mlog = logging.getLogger(__name__)
 package_path: Path = Path(__file__).parent
 """Package file system path"""
 
-try:
-    json_schema_repo: json.SchemaRepository = json.SchemaRepository(
-        hat.monitor.common.json_schema_repo,
-        json.SchemaRepository.from_json(
-            package_path / 'json_schema_repo.json'))
-    """JSON schema repository"""
-except Exception as e:
-    mlog.error('could not load JSON schema, AIMM will not work properly: %s',
-               e, exc_info=e)
-    json_schema_repo: json.SchemaRepository = json.SchemaRepository(
-        {'id': 'placeholder'})
+json_schema_repo: json.SchemaRepository = json.SchemaRepository(
+    hat.monitor.common.json_schema_repo,
+    json.SchemaRepository.from_json(
+        package_path / 'json_schema_repo.json'))
+"""JSON schema repository"""
 
 
 JSON = typing.Union[None, bool, int, float, str, typing.List['JSON'],
