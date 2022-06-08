4908c21c0a920f94174195ff6b9230bae37d546c
mateo <mateo1559@gmail.com>
2022-06-08 09:03:05 +0200

    removed deprecated f., added install.sh

---
+++ examples/0003/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# curl https://raw.githubusercontent.com/numenta/NAB/master/data/realKnownCause/ambient_temperature_system_failure.csv --output dataset/ambient_temperature_system_failure.csv --silent
+
+yarn install --silent
+node_modules/.bin/webpack --config webpack.config.js
+
+
--- examples/0003/run.sh
+++ examples/0003/run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# napravi curl za dataset
-yarn install --silent
-node_modules/.bin/webpack --config webpack.config.js
-# gore ide posebno
 
 rm data/aimm.db -f
 export PYTHONPATH=src_py
