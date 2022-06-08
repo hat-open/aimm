2a613383b30acd89e157e31b3477102f0788ef63
mateo <mateo1559@gmail.com>
2022-06-08 09:47:40 +0200

    datasets removed,added curl to install.sh

--- examples/0003/install.sh
+++ examples/0003/install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# curl https://raw.githubusercontent.com/numenta/NAB/master/data/realKnownCause/ambient_temperature_system_failure.csv --output dataset/ambient_temperature_system_failure.csv --silent
+dataset_setup () {
+    mkdir -p dataset
+    curl https://archive.ics.uci.edu/ml/machine-learning-databases/00360/AirQualityUCI.zip --output dataset/AirQualityUCI.zip --silent
+    unzip -o dataset/AirQualityUCI.zip -d dataset -x "AirQualityUCI.xlsx"
+    rm -f dataset/AirQualityUCI.zip
 
+    awk -F';' '{print $4","$13","$14","$15}'  dataset/AirQualityUCI.csv > dataset/sanatized_.csv
+    cut -d, -f1-4 dataset/sanatized_.csv > dataset/sanatized.csv
+
+    rm -f dataset/AirQualityUCI.csv
+    rm -f dataset/sanatized_.csv
+}
+
+dataset_setup
 yarn install --silent
 node_modules/.bin/webpack --config webpack.config.js
 
