f7b2221e3bc1fe6242206202a0bef60438407f07
zlatsic <zlatan.sicanica@gmail.com>
2023-02-10 23:30:00 +0100

    example 2 docker wip

--- examples/0002/src_py/hat/module.py
+++ examples/0002/src_py/hat/module.py
@@ -16,10 +16,8 @@ type: object
 ...
 """, format=json.Format.YAML))
 
-_source_id = 0
 
-
-async def create(conf, engine):
+async def create(conf, engine, source):
     module = Module()
     module._gw_prefix = ('gateway', 'gateway', 'device', 'device')
     module._subscription = common.Subscription(
@@ -29,11 +27,7 @@ async def create(conf, engine):
          ('aimm', 'response')])
 
     global _source_id
-    module._source = common.Source(
-        type=common.SourceType.MODULE,
-        name=__name__,
-        id=_source_id)
-    _source_id += 1
+    module._source = source
 
     module._async_group = aio.Group()
     module._engine = engine
@@ -58,73 +52,62 @@ class Module(common.Module):
     def subscription(self):
         return self._subscription
 
-    async def create_session(self):
-        session = ModuleSession()
+    async def process(self, source, e):
+        if source == self._source:
+            return
 
-        session._async_group = self._async_group.create_subgroup()
-        session._source = self._source
-        session._engine = self._engine
-        session._module = self
+        payload = e.payload.data
+        if e.event_type == (*self._gw_prefix, 'gateway', 'running'):
+            if payload is False:
+                yield _register_event(
+                    ('gateway', 'gateway', 'device', 'device', 'system', 'enable'),
+                    True)
 
-        return session
+        elif e.event_type[0] == 'measurement':
+            self._measurements = json.set_(
+                self._measurements, list(e.event_type[1:]), payload)
+            if self._predict_task is None:
+                self._predict_task = self.async_group.spawn(self._predict, self._source)
 
-    def process(self, events):
-        for e in events:
-            if e.source == self._source:
-                continue
+        elif e.event_type == ('aimm', 'state'):
+            if ((self._model_id is None
+                    or self._model_id not in payload['models'])
+                    and self._create_model_request_id is None):
+                self._model_id = None
+                request_ev = _register_event(
+                    ('aimm', 'create_instance'),
+                    {'model_type': 'aimm_plugins.power.StateEstimator',
+                        'args': [], 'kwargs': {}})
+                request_ev_id = request_ev.event_id
+                self._create_model_request_id = request_ev_id._asdict()
+                yield request_ev
 
-            payload = e.payload.data
-            if e.event_type == (*self._gw_prefix, 'gateway', 'running'):
-                if payload is False:
-                    yield self._process_event(('gateway', 'gateway',
-                                               'device', 'device',
-                                               'system', 'enable'), True)
+        elif e.event_type == ('aimm', 'response'):
+            if payload['request_id'] == self._create_model_request_id:
+                self._model_id = str(payload['result'])
+            elif payload['request_id'] == self._predict_request_id:
+                result = payload['result']
+                if result is None:
+                    return
+                bus_ids = (set(result['vm_pu']) | set(result['va_degree'])
+                            | set(result['p_mw']) | set(result['q_mvar']))
+                for bus_id in bus_ids:
+                    yield _register_event(
+                        ('estimation', bus_id, 'v'), result['vm_pu'][bus_id])
+                    yield _register_event(
+                        ('estimation', bus_id, 'va'), result['va_degree'][bus_id])
+                    yield _register_event((
+                        'estimation', bus_id, 'p'), result['p_mw'][bus_id])
+                    yield self._register_event(
+                        ('estimation', bus_id, 'q'), result['q_mvar'][bus_id])
 
-            elif e.event_type[0] == 'measurement':
-                self._measurements = json.set_(
-                    self._measurements, list(e.event_type[1:]), payload)
-                if self._predict_task is None:
-                    self._predict_task = self.async_group.spawn(self._predict)
-
-            elif e.event_type == ('aimm', 'state'):
-                if ((self._model_id is None
-                     or self._model_id not in payload['models'])
-                        and self._create_model_request_id is None):
-                    self._model_id = None
-                    request_ev = self._process_event(
-                        ('aimm', 'create_instance'),
-                        {'model_type': 'aimm_plugins.power.StateEstimator',
-                         'args': [], 'kwargs': {}})
-                    request_ev_id = request_ev.event_id
-                    self._create_model_request_id = request_ev_id._asdict()
-                    yield request_ev
-
-            elif e.event_type == ('aimm', 'response'):
-                if payload['request_id'] == self._create_model_request_id:
-                    self._model_id = str(payload['result'])
-                elif payload['request_id'] == self._predict_request_id:
-                    result = payload['result']
-                    if result is None:
-                        continue
-                    bus_ids = (set(result['vm_pu']) | set(result['va_degree'])
-                               | set(result['p_mw']) | set(result['q_mvar']))
-                    for bus_id in bus_ids:
-                        yield self._process_event(('estimation', bus_id, 'v'),
-                                                  result['vm_pu'][bus_id])
-                        yield self._process_event(('estimation', bus_id, 'va'),
-                                                  result['va_degree'][bus_id])
-                        yield self._process_event(('estimation', bus_id, 'p'),
-                                                  result['p_mw'][bus_id])
-                        yield self._process_event(('estimation', bus_id, 'q'),
-                                                  result['q_mvar'][bus_id])
-
-    async def _predict(self):
+    async def _predict(self, source):
         await asyncio.sleep(1)  # buffer measurements
         try:
             if self._model_id is None:
                 return
             ev = await self._engine.register(
-                self._source,
+                source,
                 [_register_event(
                     ('aimm', 'predict', self._model_id),
                     {'args': [list(_measurements_to_arg(self._measurements))],
@@ -133,14 +116,6 @@ class Module(common.Module):
             self._predict_request_id = ev[0].event_id._asdict()
         finally:
             self._predict_task = None
-
-    def _process_event(self, event_type, payload):
-        return self._engine.create_process_event(
-            self._source, common.RegisterEvent(
-                event_type=event_type,
-                source_timestamp=None,
-                payload=common.EventPayload(common.EventPayloadType.JSON,
-                                            payload)))
 
 
 def _measurements_to_arg(measurements):
@@ -152,16 +127,6 @@ def _measurements_to_arg(measurements):
                    'std_dev': 10,
                    'element': int(bus_id),
                    'side': None}
-
-
-class ModuleSession(common.ModuleSession):
-
-    @property
-    def async_group(self):
-        return self._async_group
-
-    async def process(self, changes):
-        return self._module.process(changes)
 
 
 def _register_event(event_type, payload):
