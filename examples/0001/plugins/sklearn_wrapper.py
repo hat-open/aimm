706c1353105676a0c106d28866435241762d8269
zlatsic <zlatan.sicanica@gmail.com>
2024-09-19 16:50:05 +0200

    example 1 improvements

--- examples/0001/plugins/sklearn_wrapper.py
+++ examples/0001/plugins/sklearn_wrapper.py
@@ -1,23 +1,23 @@
 from aimm import plugins
-import sklearn.svm
-import sklearn.datasets
+from sklearn import svm
+from sklearn import datasets
 import pickle
 
 
 @plugins.data_access("iris_inputs")
 def iris_inputs():
-    return sklearn.datasets.load_iris(return_X_y=True)[0]
+    return datasets.load_iris(return_X_y=True)[0]
 
 
 @plugins.data_access("iris_outputs")
 def iris_outputs():
-    return sklearn.datasets.load_iris(return_X_y=True)[1]
+    return datasets.load_iris(return_X_y=True)[1]
 
 
 @plugins.model
 class SVC(plugins.Model):
     def __init__(self, gamma=0.001, C=100.0):
-        self._svc = sklearn.svm.SVC(gamma=gamma, C=C)
+        self._svc = svm.SVC(gamma=gamma, C=C)
 
     def fit(self, X, y):
         self._svc = self._svc.fit(X, y)
